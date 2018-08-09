fdef14d3f7f61c6adc44ec41dff3085f1183bf11
Sdev <sridevpawar@gmail.com>
2018-08-08 23:30:35 -0700

    login and ready state

--- UiTesting/ticket.js
+++ UiTesting/ticket.js
@@ -4,4 +4,13 @@ var firefoxOptions = new firefox.Options();
 var driver = new webdriver.Builder().forBrowser('firefox')
     .setFirefoxOptions(firefoxOptions)
     .build();
-driver.get("https://google.com");
+// Go to widget
+driver.get("https://portal.office.com/support/altusupport.aspx");
+
+//Login
+driver.findElement(webdriver.By.name("loginfmt")).sendKeys("porsche@ferdinandporsche.onmicrosoft.com");
+driver.findElement(webdriver.By.id("idSIButton9")).click();
+driver.findElement(webdriver.By.name("passwd")).sendKeys("i4FHNAB8G9EP");
+driver.findElement(webdriver.By.id("idSIButton9")).click();
+driver.findElement(webdriver.By.id("idBtn_Back")).click();
+
